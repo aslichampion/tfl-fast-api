1e88e79d876648d0cc69176b98bba5b8ebda6927
Asli Champion <aslichampion@icloud.com>
2024-08-27 21:25:39 +0100

    feat: set uo docker support

--- src/api/v1/endpoints/station_departures.py
+++ src/api/v1/endpoints/station_departures.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends
-
 from src.services.tracker_net import Line, TrackerNetService
 
 router = APIRouter()
@@ -9,8 +8,6 @@ router = APIRouter()
 async def station_departures(
     line_code: Line, station_code: str, service: TrackerNetService = Depends()
 ):
-    if line_code not in Line:
-        return "oops"
     return await service.station_departures(
         line_code=line_code, station_code=station_code
     )
--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from typing import Optional
 from fastapi import FastAPI
 
 from src.api.v1.endpoints import station_departures
@@ -6,14 +5,8 @@ from src.api.v1.endpoints import station_departures
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/ping")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[int] = None):
-    return {"item_id": item_id, "q": q}
-
+    return {"ping": "pong"}
 
 app.include_router(station_departures.router)
